2089ee34ab0943424049b7331b35dc5599ebf2a5
Danillo Oliveira <danillo.alves.o@gmail.com>
2025-12-15 20:17:34 -0300

    feat: supabase module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), AuthModule, SupabaseModule],
   controllers: [],
   providers: [],
 })
